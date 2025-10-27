746df430862561dd0836a022d4aaea38aacf7535
unknown <7b7bruno@gmail.com>
2025-10-27 10:46:27 +0200

    Added ticket-price.js

---
+++ ticket-price.js
@@ -0,0 +1,9 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if(visitorAge <= 6)
+    console.log("Ieeja ir bez maksas!");
+else if(isStudent)
+    console.log("Cena ir 5 EUR.");
+else
+    console.log("Cena ir 12 EUR.");

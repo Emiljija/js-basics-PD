b65531442b7e81c9c6be6afd710a33a61b81956f
unknown <7b7bruno@gmail.com>
2025-10-27 11:03:24 +0200

    Added people-array.js

---
+++ people-array.js
@@ -0,0 +1,6 @@
+let people = [
+  { name: "Jānis", age: 16, isStudent: true },
+  { name: "Jolanta", age: 21, isStudent: false },
+];
+
+for (let i = 0; i < people.length; i++) console.log("Vārds:", people[i].name);
